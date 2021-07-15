040f08a5fa0562f0f6628c28ef0cb33eb0a8cd9b
ealbiol <eduard.albiol@gmail.com>
2021-07-15 19:17:36 +0200

    Update user.js

--- models/user.js
+++ models/user.js
@@ -15,55 +15,56 @@ let Schema = mongoose.Schema                                    //---> Creamos u
 
 
 const validRoles = {
-    values : ["ADMIN", "USER"],                                 //---> roles de acceso con más o menos privilegios de acceso.
+    values: ["ADMIN", "USER"],                                 //---> roles de acceso con más o menos privilegios de acceso.
     message: "{VALUE} is not a valid role."                     //---> Mensaje que aparecerá si el rol que mete el cliente no existe.
 }
 
 let userSchema = new Schema({                                   //---> Creamos el Schema para la colección users.
-                                                                //---> Dentro del Schema ponemos todas las propiedades que tendrá está entidad/colección (user). 
-        username: {
-            type: String,
-            unique: true,                                       //---> Si el email ya existe saltará un error informando que este email ya existe.
-            required: [true, "Username is required"]            //---> Si no ponemos required por defecto no lo será. El string después del true es el mensaje de error que aparecerá si el usuario no pone el username.
-        },
-        
-        email: {
-            type: String,
-            required: [true, "Email is required"]
-        },
+    //---> Dentro del Schema ponemos todas las propiedades que tendrá está entidad/colección (user). 
+    username: {
+        type: String,
+        //unique: true,  //change                                     //---> Si el email ya existe saltará un error informando que este email ya existe.
+        required: [true, "Username is required"]            //---> Si no ponemos required por defecto no lo será. El string después del true es el mensaje de error que aparecerá si el usuario no pone el username.
+    },
 
-        password: {
-            type: String,
-            required: [true, "Password is required"]
-        },
+    email: {
+        type: String,
+        unique: true, //change
+        required: [true, "Email is required"]
+    },
 
-        role: {
-            type: String,
-            default: "USER",                                    //---> Tipo de rol. Admin, user, etc
-            enum: validRoles                                    //---> Así sabe los roles validos que hemos definido en la variable validRoles más arriba.
-        },
+    password: {
+        type: String,
+        required: [true, "Password is required"]
+    },
 
-        active: {                                               //---> usuario desactivado pero no eliminado. active: true o active: false A efectos practicos parece como eliminado pero solo lo tenemos desactivado. Es una buena práctiva ya que borrar es muy destructivo.
-             type: Boolean,
-             default: true                                                   
-        }
-});                                 
+    role: {
+        type: String,
+        default: "USER",                                    //---> Tipo de rol. Admin, user, etc
+        enum: validRoles                                    //---> Así sabe los roles validos que hemos definido en la variable validRoles más arriba.
+    },
+
+    active: {                                               //---> usuario desactivado pero no eliminado. active: true o active: false A efectos practicos parece como eliminado pero solo lo tenemos desactivado. Es una buena práctiva ya que borrar es muy destructivo.
+        type: Boolean,
+        default: true
+    }
+});
 
 
 //Función para que oculta un campo al usuario cuando se transforma a JSON (ejemplo: contraseña, __v)
-userSchema.methods.toJSON = function(){                                                           //---> función que devuelve todos los campos guardados en formato JSON.
+userSchema.methods.toJSON = function () {                                                           //---> función que devuelve todos los campos guardados en formato JSON.
     const user = this;                                                                            //---> Evitamos solo que se devuelva el password al cliente. Por lo que dará los otros campos como username, email etc pero no mostrará la constraseña.
-    
+
     const userObject = user.toObject();
 
     delete userObject.password;
-    delete userObject.__v;                              
+    delete userObject.__v;
 
     return userObject;
 };
 //
 
 
-userSchema.plugin(uniqueValidator, {message: "{PATH} should be unique"})                          //---> Ejecutar unique validator
+userSchema.plugin(uniqueValidator, { message: "{PATH} should be unique" })                          //---> Ejecutar unique validator
 
 module.exports = mongoose.model("User", userSchema);                                              //---> Exportamos hacia fuera el modelo que acabamos de crear.
